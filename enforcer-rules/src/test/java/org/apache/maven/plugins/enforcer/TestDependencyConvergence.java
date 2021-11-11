5b64c51cb7bfea376e8698300644ff8f849919ef
Michiel Kalkman <michiel.kalkman@atos.net>
2021-11-11 20:53:18 +0100

    Better name test setup

--- enforcer-rules/src/test/java/org/apache/maven/plugins/enforcer/TestDependencyConvergence.java
+++ enforcer-rules/src/test/java/org/apache/maven/plugins/enforcer/TestDependencyConvergence.java
@@ -30,7 +30,7 @@ import java.io.IOException;
 @RunWith( Enclosed.class )
 public class TestDependencyConvergence {
 
-    public static class ExcludesDoNotUseTransitiveDependencies
+    public static class DoNotAllowSameArtifactDifferentVersion
     {
         private DependencyConvergenceTestSetup setup;
 
